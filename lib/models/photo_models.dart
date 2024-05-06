1e4404e76e555fad2228cb0f388b24f29f3c9d52
mmehedihasan <mmehedihasansagar@gmail.com>
2024-05-06 11:22:30 +0600

    Photo Model dart page create

--- lib/models/photo_models.dart
+++ lib/models/photo_models.dart
@@ -1,5 +1,31 @@
 class PhotoModel {
   String? url;
+  SrcModel? src;
 
-  PhotoModel({this.url});
+  PhotoModel({this.url, this.src});
+
+  factory PhotoModel.fromMap(Map<String, dynamic> parsedJson) {
+    return PhotoModel(
+      url: parsedJson['url'],
+      src: SrcModel.fromMap(parsedJson['src']),
+    );
+  }
+}
+
+class SrcModel {
+  String? portrait;
+  String? landscape;
+  String? large;
+  String? medium;
+
+  SrcModel({this.portrait, this.landscape, this.large, this.medium});
+
+  factory SrcModel.fromMap(Map<String, dynamic> srcJson) {
+    return SrcModel(
+      portrait: srcJson['portrait'],
+      landscape: srcJson['landscape'],
+      large: srcJson['large'],
+      medium: srcJson['medium'],
+    );
+  }
 }
