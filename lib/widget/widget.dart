ccc7689f093f0f060bde9f6d168dc47618dfe917
mmehedihasan <mmehedihasansagar@gmail.com>
2024-05-09 22:36:44 +0600

    some Change

--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -4,14 +4,14 @@ import 'package:wallpaper/models/photo_models.dart';
 
 Widget walpaper(List<PhotoModel> listPhotos, BuildContext context) {
   return Container(
-    padding: EdgeInsets.symmetric(horizontal: 16),
+    padding: const EdgeInsets.symmetric(horizontal: 16),
     child: GridView.count(
-        padding: EdgeInsets.all(4.0),
+        padding: const EdgeInsets.all(4.0),
         crossAxisCount: 2,
         childAspectRatio: 0.6,
         mainAxisSpacing: 6.0,
         crossAxisSpacing: 6.0,
-        physics: ClampingScrollPhysics(),
+        physics: const ClampingScrollPhysics(),
         shrinkWrap: true,
         scrollDirection: Axis.vertical,
         children: listPhotos.map((PhotoModel photoModel) {
